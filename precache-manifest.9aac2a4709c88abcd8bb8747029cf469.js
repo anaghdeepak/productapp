28e2b16504dfb31434ed9b3fbe4e986a7e5cb1c7
anaghdeepak <sruthilohi@gmail.com>
2019-03-11 15:53:12 +0400

    Updates

---
+++ precache-manifest.9aac2a4709c88abcd8bb8747029cf469.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a868766a6208325b1f8b",
+    "url": "/productapp/static/js/runtime~main.a868766a.js"
+  },
+  {
+    "revision": "223436731a8bdf9036d2",
+    "url": "/productapp/static/js/main.22343673.chunk.js"
+  },
+  {
+    "revision": "4e001cf2adc591b85471",
+    "url": "/productapp/static/js/2.4e001cf2.chunk.js"
+  },
+  {
+    "revision": "223436731a8bdf9036d2",
+    "url": "/productapp/static/css/main.f2456698.chunk.css"
+  },
+  {
+    "revision": "4e001cf2adc591b85471",
+    "url": "/productapp/static/css/2.d117122d.chunk.css"
+  },
+  {
+    "revision": "7c855665d33cc2e3f0c5e673f503067a",
+    "url": "/productapp/index.html"
+  }
+];
